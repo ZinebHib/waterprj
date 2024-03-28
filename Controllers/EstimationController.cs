c53f06cac14e6e6962c8e7e5505178bc73f77a6e
ZinebHib <HIBAOUI_ZINEB@emsi-edu.ma>
2024-03-28 12:25:49 +0000

    estimation page version

--- Controllers/EstimationController.cs
+++ Controllers/EstimationController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -13,18 +14,75 @@ namespace waterprj.Controllers
     public class EstimationController : Controller
     {
         private readonly ApplicationDbContext _context;
+        //added for userid
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public EstimationController(ApplicationDbContext context)
+        public EstimationController(ApplicationDbContext context , IHttpContextAccessor httpContextAccessor)
         {
             _context = context;
+            //added for userid
+            _httpContextAccessor = httpContextAccessor;
         }
 
         // GET: Estimation
-        public async Task<IActionResult> Index()
+          public async Task<IActionResult> Index()
+           {
+                 return _context.Estimation != null ? 
+                             View(await _context.Estimation.ToListAsync()) :
+                             Problem("Entity set 'ApplicationDbContext.Estimation'  is null.");
+           }
+        //to calculate the estimation based on user's behavior :
+        // POST: Estimation/CalculateAndSaveEstimation
+        // POST: Estimation/CalculateAndSaveEstimation
+        // POST: Estimation/CalculateAndSaveEstimation
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CalculateAndSaveEstimation([Bind("NumberOfPeople,HasPool,UsesDishwasher,LaundryFrequency,ShowerDuration,LeakDetection")] Estimation estimation)
         {
-              return _context.Estimation != null ? 
-                          View(await _context.Estimation.ToListAsync()) :
-                          Problem("Entity set 'ApplicationDbContext.Estimation'  is null.");
+            if (ModelState.IsValid)
+            {
+                
+
+                estimation.Date = DateTime.Now; // Set the current date and time
+
+                // Perform estimation calculation
+                double estimatedVolume = CalculateEstimation(estimation);
+
+                // Set the calculated estimation volume
+                estimation.EstimatedVolume = estimatedVolume;
+
+                // Save the estimation to the database
+                _context.Estimation.Add(estimation);
+                await _context.SaveChangesAsync();
+
+                // Pass the estimation result to TempData
+                TempData["EstimationResult"] = estimatedVolume.ToString();
+
+                // Redirect to the index page
+                return RedirectToAction(nameof(Index));
+            }
+
+            // If ModelState is not valid, return to the Create view to correct errors
+            return View("Create", estimation);
+        }
+
+
+        // Method to get the current user's ID
+       
+
+
+
+        // Method to calculate the estimation
+        private double CalculateEstimation(Estimation estimation)
+        {
+            // Implement your estimation calculation logic here
+           
+            double baseVolume = 100; // Base consumption volume
+            double poolFactor = estimation.HasPool ? 50 : 0; // Additional volume if the user has a pool
+            double dishwasherFactor = estimation.UsesDishwasher ? 30 : 0; // Additional volume if the user uses a dishwasher
+                                                                        
+            double totalVolume = baseVolume + (estimation.NumberOfPeople * 50) + poolFactor + dishwasherFactor;
+            return totalVolume;
         }
 
         // GET: Estimation/Details/5
