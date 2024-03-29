48674477526cabc3d01dfef19ca51c0e1e0b3d9f
ZinebHib <HIBAOUI_ZINEB@emsi-edu.ma>
2024-03-29 22:50:03 +0000

    authorization done

--- obj/Debug/net6.0/waterprj.AssemblyInfo.cs
+++ obj/Debug/net6.0/waterprj.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("waterprj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+257dae21a1c854e4517a6cd4ee59113a701bb791")]
 [assembly: System.Reflection.AssemblyProductAttribute("waterprj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("waterprj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
